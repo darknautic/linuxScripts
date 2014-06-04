df2f8d044ef7a3afbc80786bdab30c370ab4e163
darkNautic <blue.darknautic@gmail.com>
2014-06-03 20:39:46 -0500

    Update composedCommands.sh

--- composedCommands.sh
+++ composedCommands.sh
@@ -1,3 +1,9 @@
 ## CSV
 ######
+find ./  -type f  -print0 | xargs -0 md5sum | awk '{ printf $1","; for (i=2; i <= NF; i++) printf FS$i; print NL}'
+
 find ./  -type f  -print0 | xargs -0 md5sum | awk '{printf  "\"" ;for (i=2; i <= NF; i++) printf $i; printf "\"" FS "," FS "\""$1"\"" ; print NL}'
+
+
+
+
