6cd730b71e59ecb255e4d0965c03e96b385309fc
darkNautic <blue.darknautic@gmail.com>
2014-08-27 19:43:16 -0500

    Update Now.sh

--- Now.sh
+++ Now.sh
@@ -2,5 +2,9 @@
 
 #  Now 
 
+## Feel free to use, modify and distribute this script as well as request  modify 
+## privilege on this repository to improve it and make it more useful and valuable.
+## blue.darkNautic@gmail.com
+
 NOW=$(date +"%Y%m%d-%H%M");
 echo $NOW
