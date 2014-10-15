d28d4d6cdf53a0597796196c39551216b130c038
darkNautic <blue.darknautic@gmail.com>
2014-10-15 18:15:42 -0500

    Update scanNetwork.sh

--- scanNetwork.sh
+++ scanNetwork.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 # https://github.com/darknautic/linuxScripts.git
+## Feel free to use, modify and distribute this script as well as request  modify 
+## privilege on this repository to improve it and make it more useful and valuable.
+## blue.darkNautic@gmail.com
+
 echo -e "\n scannig network .....\n"
 
 pingFunction()
