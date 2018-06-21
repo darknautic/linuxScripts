bde5be2c486b331921a1ae6db3b35bcee9bf61e8
darkNautic <blue.darknautic@gmail.com>
2018-06-21 12:37:50 -0500

    adding joins utility

---
+++ joins.bash
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+## script name : joins
+## darkNautic
+##
+## options : left, right, outer, join
+##
+
+option=$1
+file1=$2
+file2=$3
+
+if [[ "${option}" == "left" ]];then
+  echo "[INFO] - Left Join"
+  while read line ;do if [[ $(grep -cw "${line}" ${file2}) -le 0 ]]; then echo ${line} ;fi ; done< ${file1}
+elif [[ "${option}" == "right" ]];then
+ echo "[INFO] - Right Join"
+ while read line ;do if [[ $(grep -cw "${line}" ${file1}) -le 0 ]]; then echo ${line} ;fi ; done< ${file2}
+elif [[ "${option}" == "outer" ]];then
+ echo "[INFO] - Outer Join"
+ while read line ;do if [[ $(grep -cw "${line}" ${file2}) -le 0 ]]; then echo ${line} ;fi ; done< ${file1}
+ while read line ;do if [[ $(grep -cw "${line}" ${file1}) -le 0 ]]; then echo ${line} ;fi ; done< ${file2}
+elif [[ "${option}" == "join" ]];then
+ echo "[INFO] - Simply prints the matchess."
+ while read line ;do if [[ $(grep -cw "${line}" ${file2}) -gt 0 ]]; then echo ${line} ;fi ; done< ${file1}
+else
+ echo "[ERROR] : \"left\" \"right\"  \"outer\"  \"join\" "
+fi
+
+#while read line ;do if [[ $(grep -cw "${line}" f1.txt) -gt 0 ]]; then echo ${line} ;else echo -e " * "${line} ;fi ; done< f2.txt
--- useful_commands.bash
+++
@@ -1,2 +0,0 @@
-# to compare to files regardless the orden ,those tagged with * are missing in file1
-while read line ;do if [[ $(grep -cw "${line}" file1) -gt 0 ]]; then echo ${line} ;else echo -e " * "${line} ;fi ; done< file2
